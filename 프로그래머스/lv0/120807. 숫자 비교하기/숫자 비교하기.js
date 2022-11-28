e107e7c56682f3ba0b1a791cb11f172577d14ec2
lunadein2022 <115081038+lunadein2022@users.noreply.github.com>
2022-11-28 20:19:49 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    let answer = 0;
+    if(num1 === num2){
+        return 1;
+    }else if(num1 !== num2){
+        return -1;
+    }
+    
+    return answer;
+}
